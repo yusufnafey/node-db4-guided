74a79fb67f115653842cc4f36d934d75579d74c7
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-15 10:24:40 -0700

    adds foreign key constraint

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,21 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     useNullAsDefault: true, // needed for sqlite
     connection: {
-      filename: './data/zoos.db3',
+      filename: "./data/zoos.db3"
     },
     migrations: {
-      directory: './data/migrations'
+      directory: "./data/migrations"
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./data/seeds"
     },
-  },
+    // sqlite will not enforce foreign keys by default
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON"); // turns on the foreign key enforcement
+      }
+    }
+  }
 };
